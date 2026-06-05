97020135d00c667d1a36dc7a4592e453ca6de3ee
r2o4life <39225923+r2o4life@users.noreply.github.com>
2026-06-05 19:29:08 +0000

    Deploying to gh-pages from @ r2o4life/artworthi@f778d94cfbbd8fca30a248cca985e12a9489b53f 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3804839118" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2622590177" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
